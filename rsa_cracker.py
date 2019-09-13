732a36a869313bc5a2e022a0d7de62b6b32a6228
abrodhead352 <abrodhead@zagmail.gonzaga.edu>
2019-09-12 17:27:54 -0700

    mostly finished

--- rsa_cracker.py
+++ rsa_cracker.py
@@ -15,8 +15,7 @@ def main():
     d = 3
     P = 5
     C = 14
-    print(rsa_encrypt(P,e,n))
-    print(rsa_decrypt(C,d,n))
+    runTest(1,e,n,P,d,C)
 
 #uses a public key 'e' to encrypt plaintext into ciphertext
 def rsa_encrypt(plaintext, e, n):
@@ -30,7 +29,10 @@ def rsa_decrypt(ciphertext, d, n):
 
 #cracks the rsa encryption and returns private key d
 def crack_rsa(e, n):
-    pass
+    p, q = getPrimeFactors(n)
+    phiN = (p-1)*(q-1)
+    d = getPrivateKey(e, phiN)
+    return d
 
 #returns false if n is divisible by any number x| 2<x<sqrt(n)
 def isPrime(n):
@@ -41,6 +43,29 @@ def isPrime(n):
 
 #gets the private key using e and phi(n)
 def getPrivateKey(e, phiN):
-    pass
+    for i in range(2, phiN):
+        if (e*i)%phiN is 1:
+            return i
+    return (-1)
+
+#runs a single test of the cracking with set e,n,P,d,and C.
+def runTest(testNum, e, n, P, d, C):
+    #get test values for d, C, and P
+    d_prime = crack_rsa(e,n)
+    C_prime = rsa_encrypt(P, e, n)
+    P_prime = rsa_decrypt(C_prime, d_prime, n)
+
+    print("**************** Test case " + str(testNum) + " ****************")
+
+
+#returns a tuple of prime factors of a number n
+def getPrimeFactors(n):
+    p = 1
+    for i in range(2, int(math.sqrt(n)+1)):
+        if isPrime(i) and n%i is 0: #i is a prime divisor of n
+            p = i
+            break
+    q = n/p
+    return (p,int(q))
 
 main()
